36a1d8630bbb27e6177cf852f14df2fee5adc5c8
Sweetsuro <rpathak2002@gmail.com>
2023-10-30 19:15:12 -0400

    enabled and ran prettier

--- src/posts/endorse.js
+++ src/posts/endorse.js
@@ -1,68 +1,71 @@
-'use strict';
+"use strict"
 
-const db = require('../database');
-const plugins = require('../plugins');
+const db = require("../database")
+const plugins = require("../plugins")
 
 module.exports = function (Posts) {
     Posts.endorse = async function (pid, uid) {
-        return await toggleEndorse('endorse', pid, uid);
-    };
+        return await toggleEndorse("endorse", pid, uid)
+    }
 
     Posts.unendorse = async function (pid, uid) {
-        return await toggleEndorse('unendorse', pid, uid);
-    };
+        return await toggleEndorse("unendorse", pid, uid)
+    }
 
     async function toggleEndorse(type, pid, uid) {
         if (parseInt(uid, 10) <= 0) {
-            throw new Error('[[error:not-logged-in]]');
+            throw new Error("[[error:not-logged-in]]")
         }
 
-        const isEndorsing = type === 'endorse';
+        const isEndorsing = type === "endorse"
 
         const [postData, hasEndorsed] = await Promise.all([
-            Posts.getPostFields(pid, ['pid', 'uid']),
-            Posts.hasEndorsed(pid, uid),
-        ]);
+            Posts.getPostFields(pid, ["pid", "uid"]),
+            Posts.hasEndorsed(pid, uid)
+        ])
 
         if (isEndorsing && hasEndorsed) {
-            throw new Error('[[error:already-endorsed]]');
+            throw new Error("[[error:already-endorsed]]")
         }
 
         if (!isEndorsing && !hasEndorsed) {
-            throw new Error('[[error:already-unendorsed]]');
+            throw new Error("[[error:already-unendorsed]]")
         }
 
         if (isEndorsing) {
-            await db.sortedSetAdd(`uid:${uid}:endorsements`, Date.now(), pid);
+            await db.sortedSetAdd(`uid:${uid}:endorsements`, Date.now(), pid)
         } else {
-            await db.sortedSetRemove(`uid:${uid}:endorsements`, pid);
+            await db.sortedSetRemove(`uid:${uid}:endorsements`, pid)
         }
-        await db[isEndorsing ? 'setAdd' : 'setRemove'](`pid:${pid}:users_endorsed`, uid);
-        postData.endorsements = await db.setCount(`pid:${pid}:users_endorsed`);
-        await Posts.setPostField(pid, 'endorsements', postData.endorsements);
+        await db[isEndorsing ? "setAdd" : "setRemove"](
+            `pid:${pid}:users_endorsed`,
+            uid
+        )
+        postData.endorsements = await db.setCount(`pid:${pid}:users_endorsed`)
+        await Posts.setPostField(pid, "endorsements", postData.endorsements)
 
         plugins.hooks.fire(`action:post.${type}`, {
             pid: pid,
             uid: uid,
             owner: postData.uid,
-            current: hasEndorsed ? 'endorsed' : 'unendorsed',
-        });
+            current: hasEndorsed ? "endorsed" : "unendorsed"
+        })
 
         return {
             post: postData,
-            isEndorsed: isEndorsing,
-        };
+            isEndorsed: isEndorsing
+        }
     }
 
     Posts.hasEndorsed = async function (pid, uid) {
         if (parseInt(uid, 10) <= 0) {
-            return Array.isArray(pid) ? pid.map(() => false) : false;
+            return Array.isArray(pid) ? pid.map(() => false) : false
         }
 
         if (Array.isArray(pid)) {
-            const sets = pid.map(pid => `pid:${pid}:users_endorsed`);
-            return await db.isMemberOfSets(sets, uid);
+            const sets = pid.map((pid) => `pid:${pid}:users_endorsed`)
+            return await db.isMemberOfSets(sets, uid)
         }
-        return await db.isSetMember(`pid:${pid}:users_endorsed`, uid);
-    };
-};
+        return await db.isSetMember(`pid:${pid}:users_endorsed`, uid)
+    }
+}
